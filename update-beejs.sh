faacd654986638fc4718716170084a17189ed667
Adam Uhlíř <adam@uhlir.dev>
2022-05-26 15:49:27 +0200

    feat: update script (#28)
    * feat: update script
    
    * fix: use fixed bee-js version

---
+++ update-beejs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+beeJsVersion=$1
+
+if [ -z "$beeJsVersion" ]; then
+    echo "No bee-js version!"
+    exit 1
+fi
+
+find . -name 'package.json' -maxdepth 2 -type f -print0 | while read -d $'\0' file
+do
+  sed -i '' "s/\"@ethersphere\/bee-js\": \".*\",/\"@ethersphere\/bee-js\": \"${beeJsVersion}\",/g" "$file"
+  cd $(dirname $file) || exit
+  npm install
+  cd ..
+done
