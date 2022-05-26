21085a95607def7b7237751eff0cb45277b7a9f2
Adam Uhlíř <adam@uhlir.dev>
2022-05-26 13:49:35 +0200

    fix: use fixed bee-js version

--- update-beejs.sh
+++ update-beejs.sh
@@ -9,7 +9,7 @@ fi
 
 find . -name 'package.json' -maxdepth 2 -type f -print0 | while read -d $'\0' file
 do
-  sed -i '' "s/\"@ethersphere\/bee-js\": \".*\",/\"@ethersphere\/bee-js\": \"^${beeJsVersion}\",/g" "$file"
+  sed -i '' "s/\"@ethersphere\/bee-js\": \".*\",/\"@ethersphere\/bee-js\": \"${beeJsVersion}\",/g" "$file"
   cd $(dirname $file) || exit
   npm install
   cd ..
